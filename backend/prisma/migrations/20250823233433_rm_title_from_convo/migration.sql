6c38e21044da5a4e4453fb69438f3ff8e71d20e2
chirag100x <chiragchoudhary9077@gmail.com>
2025-09-07 00:07:57 +0530

    Delete backend directory

--- backend/prisma/migrations/20250823233433_rm_title_from_convo/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `title` on the `Conversation` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Conversation" DROP COLUMN "title";
